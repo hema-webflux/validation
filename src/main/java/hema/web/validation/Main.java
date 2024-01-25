0cbce72fc093212af5710f7e048285e75fbf1852
xgbnl <745502274@qq.com>
2024-01-25 18:03:47 +0800

    chore: 完善部份验证方法

--- src/main/java/hema/web/validation/Main.java
+++ src/main/java/hema/web/validation/Main.java
@@ -4,24 +4,26 @@ package hema.web.validation;
 import hema.web.validation.contracts.ValidateRule;
 import hema.web.validation.proxy.ValidateRuleProxyConfiguration;
 
+import java.net.MalformedURLException;
+import java.net.URI;
 import java.util.Map;
 import java.util.Set;
 
 //@SpringBootApplication
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
 
-        ValidateRule validateRule = new ValidateRuleProxyConfiguration().validateRule();
+//        ValidateRule validateRule = new ValidateRuleProxyConfiguration().validateRule();
+//
+//        validateRule.field("name").after("date").before("age").max(20).min(10).exists("users", "name").unique("users", "name");
+//
+//        validateRule.field("email").required().email().unique("accounts", "email").tryEnum(Toggle.class);
+//
+//        Map<String, Set<ValidateRule.Access>> rules = validateRule.rules();
+//
+//        rules.get("name").forEach(ele ->System.out.println(ele.first()));
+//        rules.get("email").forEach(ele ->System.out.println(ele.first()));
 
-        validateRule.field("name").after("date").before("age").max(20).min(10).exists("users", "name").unique("users", "name");
 
-        validateRule.field("email").required().email().unique("accounts", "email").isEnum(Toggle.class);
-
-        Map<String, Set<ValidateRule.Access>> rules = validateRule.rules();
-
-        rules.get("name").forEach(ele ->System.out.println(ele.first()));
-        rules.get("email").forEach(ele ->System.out.println(ele.first()));
-    }
-
-    enum Toggle {}
+        }
 }
--- src/main/java/hema/web/validation/contracts/ValidateAttributes.java
+++ src/main/java/hema/web/validation/contracts/ValidateAttributes.java
@@ -1,7 +1,6 @@
 package hema.web.validation.contracts;
 
-import java.util.HashSet;
-import java.util.List;
+import java.net.URI;
 import java.util.Map;
 import java.util.Set;
 
@@ -12,6 +11,35 @@ public interface ValidateAttributes {
         Set<Object> acceptable = Set.of("yes", "on", "1", 1, true, "true");
 
         return validateRequired(attribute, value) && acceptable.contains(value);
+    }
+
+    default <T> boolean validateDeclined(String attribute, T value) {
+
+        Set<Object> acceptable = Set.of("no", "off", "0", 0, false, "false");
+
+        return validateRequired(attribute, value) && acceptable.contains(value);
+    }
+
+    default <T> boolean validateUrl(String attribute, T value) {
+
+        if (!validateString(attribute, value)) {
+            return false;
+        }
+
+        try {
+            return !URI.create((String) value).toURL().toString().isEmpty();
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    default <T> boolean validateString(String attribute, T value) {
+
+        if (!(value instanceof String)) {
+            return false;
+        }
+
+        return !((String) value).isEmpty() || !((String) value).isBlank();
     }
 
     default <T> boolean validateRequired(String attribute, T value) {
@@ -33,5 +61,9 @@ public interface ValidateAttributes {
 
     default <T> boolean validateNumeric(T value) {
         return value instanceof Number;
+    }
+
+    default <T> boolean validateTryEnum(String attribute, T value, Class<T> type) {
+        return type.isInstance(value);
     }
 }
