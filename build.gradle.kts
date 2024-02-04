4f9dbc2a021fd94795e1a7e28cc62b43fb981b37
xgbnl <745502274@qq.com>
2024-02-04 10:45:23 +0800

    chore: gradle upgrade.

--- build.gradle.kts
+++ build.gradle.kts
@@ -75,6 +75,15 @@ publishing {
     }
 }
 
+dependencies {
+    implementation("org.json:json:20231013")
+    implementation("org.springframework.boot:spring-boot-starter-web:3.2.2")
+    implementation("com.google.code.findbugs:jsr305:3.0.2")
+    implementation("io.github.hema-webflux:contracts:1.1")
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
 signing {
     sign(publishing.publications["mavenJava"])
 }
