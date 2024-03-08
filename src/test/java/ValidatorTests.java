8e2d7b936a2c6df1777b4ffba3c2bac4cc66d90c
xgbnl <745502274@qq.com>
2024-03-08 15:50:46 +0800

    tests

--- src/test/java/ValidatorTests.java
+++ src/test/java/ValidatorTests.java
@@ -1,21 +1,25 @@
 import hema.web.validation.FormValidator;
 import hema.web.validation.contracts.ValidateRule;
-import org.springframework.context.ApplicationContext;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class ValidatorTests extends FormValidator {
-    public ValidatorTests(ApplicationContext container) {
-        super(container);
-    }
 
     @Override
     protected void rules(ValidateRule rule) {
-        rule.field("name").required().string();
-        rule.field("email").required().email();
-        rule.field("phone").required().phone().unique("users", "phone");
+        rule.add("name").required().string();
+        rule.add("email").required().email();
+        rule.add("phone").required().phone().unique("users", "phone");
     }
 
     @Override
     protected boolean authorize() {
         return false;
     }
+
+    @Test
+    public void test() {
+        Assertions.assertEquals("name", "name");
+    }
+
 }
