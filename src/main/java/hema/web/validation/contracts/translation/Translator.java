331782858f0dded242755267ae9c60c84478f4e4
xgbnl <745502274@qq.com>
2024-03-09 17:18:30 +0800

    feat(Translation): 新增转换器

---
+++ src/main/java/hema/web/validation/contracts/translation/Translator.java
@@ -0,0 +1,16 @@
+package hema.web.validation.contracts.translation;
+
+import org.springframework.lang.Nullable;
+
+import java.util.Map;
+
+public interface Translator {
+
+    String get(String key, Map<String, Object> replace, @Nullable String locale);
+
+    String choice(String key);
+
+    String getLocale();
+
+    void setLocale(String locale);
+}
---
+++ src/main/java/hema/web/validation/translation/Translator.java
@@ -0,0 +1,37 @@
+package hema.web.validation.translation;
+
+import hema.web.validation.contracts.translation.Loader;
+
+import java.util.Map;
+
+public class Translator implements hema.web.validation.contracts.translation.Translator {
+
+    private final Loader loader;
+
+    private final String locale;
+
+    Translator(Loader loader, String locale) {
+        this.loader = loader;
+        this.locale = locale;
+    }
+
+    @Override
+    public String get(String key, Map<String, Object> replace, String locale) {
+        return null;
+    }
+
+    @Override
+    public String choice(String key) {
+        return null;
+    }
+
+    @Override
+    public String getLocale() {
+        return null;
+    }
+
+    @Override
+    public void setLocale(String locale) {
+
+    }
+}
