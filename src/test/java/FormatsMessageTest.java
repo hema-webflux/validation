11b74218d40f7c77d99c00164651959e8c2776ac
xgbnl <745502274@qq.com>
2024-03-12 17:02:20 +0800

    test

---
+++ src/test/java/FormatsMessageTest.java
@@ -0,0 +1,42 @@
+import hema.web.validation.concerns.FormatsMessages;
+import hema.web.validation.concerns.store.MessageSource;
+import hema.web.validation.contracts.source.SimpleSource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class FormatsMessageTest {
+
+    private static final FormatsMessages formatsMessages = new Format();
+
+    private static final SimpleSource simpleSource = new MessageSource();
+
+    @BeforeAll
+    public static void beforeAddRule() {
+
+        simpleSource.add("name.required", "The name is required.")
+                .add("email*", closure -> (
+                        closure.add("email", "Must be in email format.")
+                                .add("max", "Must be max length")
+                ));
+
+    }
+
+    @Test
+    public void testGetFromLocal() {
+
+        Assertions.assertEquals(formatsMessages.getFromLocalArray("name","required",simpleSource),"The name is required.");
+        Assertions.assertEquals(formatsMessages.getFromLocalArray("email_verify","email",simpleSource),"Must be in email format.");
+
+        Assertions.assertTrue(formatsMessages.getFromLocalArray("email_confirm","max",simpleSource).contains("max length"));
+    }
+
+    static class Format implements FormatsMessages {
+
+        @Override
+        public String replacePlaceholderInString(String attribute) {
+            return null;
+        }
+    }
+
+}
