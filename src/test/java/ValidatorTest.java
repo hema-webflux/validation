382632269dd46c1731658a63bffc28ee62c06986
xgbnl <745502274@qq.com>
2024-04-30 14:17:46 +0800

    test

--- src/test/java/ValidatorTest.java
+++ src/test/java/ValidatorTest.java
@@ -1,6 +1,6 @@
 import hema.web.validation.RequestValidator;
-import hema.web.validation.concerns.haystack.AttributeHaystack;
-import hema.web.validation.concerns.haystack.MessageHaystack;
+import hema.web.validation.Attribute;
+import hema.web.validation.concerns.haystack.Message;
 import hema.web.validation.contracts.ValidateRule;
 
 public class ValidatorTest extends RequestValidator {
@@ -18,15 +18,17 @@ public class ValidatorTest extends RequestValidator {
     }
 
     @Override
-    protected void attributes(AttributeHaystack haystacks) {
-        haystacks.add("name", "用户名")
+    protected Attribute attributes() {
+        return Attribute.make()
+                .add("name", "用户名")
                 .add("email", "邮箱")
                 .add("phone", "手机号");
     }
 
     @Override
-    protected void messages(MessageHaystack haystacks) {
-        haystacks.add("name*", closure -> (
+    protected Message messages() {
+        return Message.make()
+                .add("name*", closure -> (
                         closure.add("required", "xxx")
                                 .add("string", "xxx")
                 ))
